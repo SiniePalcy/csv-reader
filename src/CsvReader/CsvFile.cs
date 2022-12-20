b8b296f3b7d4b8dd54b29911ea67c072c55c10be
sergei filippov <72672477+SiniePalcy@users.noreply.github.com>
2022-12-20 11:06:18 +0100

    code refactoring in csf file presenter

--- src/CsvReader/CsvFile.cs
+++ src/CsvReader/CsvFile.cs
@@ -4,16 +4,16 @@ namespace CsvReader
 {
     internal class CsvFile : ICsvFile
     {
-        private readonly List<Dictionary<string, string>> _content;
+        private readonly List<Dictionary<string, string>> _rows;
 
         public CsvFile(IEnumerable<string> headers, List<Dictionary<string, string>> content)
         {
-            _content = content ?? throw new ArgumentNullException(nameof(content))!;
+            _rows = content ?? throw new ArgumentNullException(nameof(content))!;
             Columns = headers ?? throw new ArgumentNullException(nameof(headers))!;
         }
-
-        public int RowsCount => _content.Count;
-        public IEnumerable<string> Columns { get; private set; }
-        public string this[int rowIndex, string column] => _content[rowIndex][column];
+       
+        public IEnumerable<string> Columns { get; }
+        public int RowsCount => _rows.Count;
+        public string this[int rowIndex, string column] => _rows[rowIndex][column];
     }
 }
